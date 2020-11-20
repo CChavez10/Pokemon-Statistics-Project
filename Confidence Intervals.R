560580eb54fcc3d14d94be47b200c47a3a5496f7
CChavez10 <72953031+CChavez10@users.noreply.github.com>
2020-11-20 00:04:01 -0500

    Delete Confidence Intervals.R

--- Confidence Intervals.R
+++
@@ -1,9 +0,0 @@
-library(readr)
-pokedex <- read_csv("Pokemon project/pokedex_(Update_05.20).csv")
-View(pokedex)
-xbar = mean(pokedex$height_m)
-s = sd(pokedex$height_m)
-n = 1028
-t = qt(0.95,1027)
-L = xbar - t*s/sqrt(n)
-U = xbar = + t*s/sqrt(n)
