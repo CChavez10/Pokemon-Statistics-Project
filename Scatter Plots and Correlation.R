f083fabe13c95724966e64b783148d388c72bd86
CChavez10 <72953031+CChavez10@users.noreply.github.com>
2020-12-11 14:33:10 -0500

    Delete Scatter Plots and Correlation.R

--- Scatter Plots and Correlation.R
+++
@@ -1,5 +0,0 @@
-library(readr)
-pokedex <- read_csv("Pokemon project/pokedex_(Update_05.20).csv")
-View(pokedex)
-plot(pokedex$height_m, pokedex$weight_kg, main = "Height vs Weight", xlab = "Height (meters)", ylab = "Weight (kg)")
-cor(pokedex$height_m, pokedex$weight_kg, use = "complete.obs")
